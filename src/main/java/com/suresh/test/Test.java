a875c001190d3b1f7e880dbb39aae65cc0f15185
sureshkperumal <surikperumal@gmail.com>
2015-06-16 13:41:24 -0700

    Added test class

---
+++ src/main/java/com/suresh/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.suresh.test;
+
+/**
+ * @author Nani
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
